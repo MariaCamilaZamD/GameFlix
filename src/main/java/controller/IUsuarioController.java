7691f5e2a69532ea6ef4be12791173a79c5a7a1a
Camiari <camiari.zamd@gmail.com>
2022-09-26 16:53:29 -0500

    Creación del controlador de usuario, la interface de usuario y del ServletUsuarioLogin

---
+++ src/main/java/controller/IUsuarioController.java
@@ -0,0 +1,7 @@
+package controller;
+
+public interface IUsuarioController {
+    
+    public String login(String username, String contrasena);
+    
+}
---
+++ src/main/java/controller/UsuarioController.java
@@ -0,0 +1,50 @@
+package controller;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import com.google.gson.Gson;
+
+import beans.Usuario;
+import connection.DBConnection;
+
+public class UsuarioController implements IUsuarioController{
+
+    @Override
+    public String login(String username, String contrasena) {
+        
+        Gson gson = new Gson();
+        
+        //Creación consulta SQL:
+        DBConnection con = new DBConnection();
+
+        String sql = "Select * from usuario where username = '" + username
+                + "' and contrasena = '" + contrasena + "'";
+        
+        //Creación de bloque try catch, el cual permite enviar información:
+        try {
+            Statement st = con.getConnection().createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                String nombre = rs.getString("nombre");
+                String apellidos = rs.getString("apellidos");
+                String email = rs.getString("email");
+                String celular = rs.getString("celular");
+                String direccion = rs.getString("direccion");
+                double saldo = rs.getDouble("saldo");
+                boolean premium = rs.getBoolean("premium");
+
+                Usuario usuario
+                        = new Usuario(username, contrasena, nombre, apellidos, email, celular, direccion, saldo, premium);
+                return gson.toJson(usuario);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            con.desconectar();
+        }
+
+        return "false";
+    }
+    
+}
