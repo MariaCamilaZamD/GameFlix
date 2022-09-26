d33d1e3f43f042bbe7a93c2e7c3fda49d771d75c
Camiari <camiari.zamd@gmail.com>
2022-09-26 16:59:24 -0500

    Creación del evento y la función de autentificación del usuario

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,6 +1,35 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
- */
+$(document).ready(function () {
 
+    $("#form-login").submit(function (event) {
 
+        event.preventDefault();
+        autenticarUsuario();
+    });
+
+});
+
+function autenticarUsuario() {
+
+    let username = $("#usuario").val();
+    let contrasena = $("#contrasena").val();
+
+    $.ajax({
+        type: "GET",
+        dataType: "html",
+        url: "./ServletUsuarioLogin",
+        data: $.param({
+            username: username,
+            contrasena: contrasena
+        }),
+        success: function (result) {
+            let parsedResult = JSON.parse(result);
+            if (parsedResult != false) {
+                $("#login-error").addClass("d-none");
+                let username = parsedResult['username'];
+                document.location.href = "home.html?username=" + username;
+            } else {
+                $("#login-error").removeClass("d-none");
+            }
+        }
+    });
+}
